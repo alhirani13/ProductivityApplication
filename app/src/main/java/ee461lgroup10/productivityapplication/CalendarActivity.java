81b734d3e221337007c77538e96b6f80d26337ad
Sana <sanakhateeb14@hotmail.com>
2017-03-21 12:08:17 -0500

    CalendarActivity basic layout finished (style and other buttons for later)

--- app/src/main/java/ee461lgroup10/productivityapplication/CalendarActivity.java
+++ app/src/main/java/ee461lgroup10/productivityapplication/CalendarActivity.java
@@ -2,12 +2,31 @@ package ee461lgroup10.productivityapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class CalendarActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        ListView mCalendarDayTasks;
+
+        //TODO: remove/refactor this, this is to make sure layout works
+        String[] useless = new String[] {
+                "No tasks yet",
+                "Enter a task with date to display here",
+                "Al and Dylan are cool",
+                "Please scroll",
+                "Need to figure out check boxes",
+                "Send help this is taking too long",
+                "Please WORK"
+        };
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(CalendarActivity.this, android.R.layout.simple_list_item_1, android.R.id.text1, useless);
+
+        mCalendarDayTasks = (ListView)findViewById(R.id.calendarTaskList);
+        mCalendarDayTasks.setAdapter(adapter);
     }
 }
