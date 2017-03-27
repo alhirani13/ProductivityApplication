e29ef08e84405c2581803325a6b9a6aec5490464
Sana <sanakhateeb14@hotmail.com>
2017-03-27 16:13:58 -0500

    added TaskListview to activity, strings shareable between classes (still need database)

--- app/src/main/java/ee461lgroup10/productivityapplication/TaskListActivity.java
+++ app/src/main/java/ee461lgroup10/productivityapplication/TaskListActivity.java
@@ -6,8 +6,12 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class TaskListActivity extends AppCompatActivity {
+
+    private ListView mTasks;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,8 +19,15 @@ public class TaskListActivity extends AppCompatActivity {
         setContentView(R.layout.activity_task_list);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(TaskListActivity.this, android.R.layout.simple_list_item_1, android.R.id.text1, Stringtasks.useless);
+        mTasks = (ListView)findViewById(R.id.taskListView);
+        mTasks.setAdapter(adapter);
+
+
+
+
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
