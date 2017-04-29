30b19206edc71f7e3b7ae240f292bf1f486b4f4a
Sana <sanakhateeb14@hotmail.com>
2017-04-29 15:44:33 -0500

    testing stuff (temporary log added but removed)

---
+++ app/src/test/java/ee461lgroup10/productivityapplication/DBHandlerTest.java
@@ -0,0 +1,36 @@
+package ee461lgroup10.productivityapplication;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DBHandlerTest {
+    @Before
+    public void setUp() throws Exception {
+
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void addTask() throws Exception {
+
+    }
+
+    @Test
+    public void getTasksCount() throws Exception {
+
+    }
+
+    @Test
+    public void deleteTask() throws Exception {
+
+    }
+
+}
